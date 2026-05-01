6cc840b168ba6f81db26159196a64354d405991f
amein <amein.job@gmail.com>
2026-05-01 11:35:01 +0200

    Nye Ændringer til Punkt 2.

--- app/src/main/java/com/example/lokal_begivenhedsapp/DetailActivity.java
+++ app/src/main/java/com/example/lokal_begivenhedsapp/DetailActivity.java
@@ -1,14 +1,17 @@
 package com.example.lokal_begivenhedsapp;
+
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.content.Intent;
+import android.net.Uri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
 
     TextView titleTextView, dateTextView, descriptionTextView;
-    Button backButton;
+    Button backButton, browserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +22,22 @@ public class DetailActivity extends AppCompatActivity {
         dateTextView = findViewById(R.id.dateTextView);
         descriptionTextView = findViewById(R.id.descriptionTextView);
         backButton = findViewById(R.id.backButton);
+        browserButton = findViewById(R.id.browserButton);
 
         String name = getIntent().getStringExtra("name");
         String date = getIntent().getStringExtra("date");
         String fullDescription = getIntent().getStringExtra("fullDescription");
+        String url = getIntent().getStringExtra("url");
 
         titleTextView.setText(name);
         dateTextView.setText(date);
         descriptionTextView.setText(fullDescription);
 
         backButton.setOnClickListener(v -> finish());
+
+        browserButton.setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(browserIntent);
+        });
     }
 }
